21716dd7b5932a6b64462cd3ababf0084e0b23b1
wyy1123 <44079135+wyy1123@users.noreply.github.com>
2023-05-02 17:57:49 -0500

    finished building admin area

--- btre/settings.py
+++ btre/settings.py
@@ -79,8 +79,11 @@ WSGI_APPLICATION = 'btre.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'btredb',
+        'USER': 'postgres',
+        'PASSWORD': 'Wyy13033211!',
+        'HOST':'localhost'
     }
 }
 
@@ -129,3 +132,8 @@ STATICFILES_DIRS =[
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+#Media Folder Settings:
+
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_URL = '/media/'
--- listings/models.py
+++ listings/models.py
@@ -1,3 +1,41 @@
 from django.db import models
+from datetime import datetime
+from realtors.models import Realtor
 
-# Create your models here.
+class Listing(models.Model):
+    #on_delete specifies that if realtor gets deleted the listing doenst get deleted
+    realtor = models.ForeignKey(Realtor, on_delete=models.DO_NOTHING)
+    title= models.CharField(max_length=200)
+    address= models.CharField(max_length=200)
+    city= models.CharField(max_length=100)
+    state= models.CharField(max_length=100)
+    zipcode= models.CharField(max_length=20)
+    #blank specifies that we dont require this to be filled
+    #textfield doenst need to specify length
+    description= models.TextField(blank=True)
+    price= models.IntegerField()
+    bedrooms = models.IntegerField()
+    bathrooms = models.DecimalField(max_digits=2, decimal_places=1)
+    garage = models.IntegerField(default=0)
+    sqft = models.IntegerField()
+    lot_size = models.DecimalField(max_digits=5, decimal_places=1)
+    # we want to  specify where in the media folder we want the photo to be uploaded
+    photo_main = models.ImageField(upload_to = 'photos/%Y/%m/%d/')
+    photo_1 = models.ImageField(upload_to = 'photos/%Y/%m/%d/', blank=True)
+    photo_2 = models.ImageField(upload_to = 'photos/%Y/%m/%d/', blank=True)
+    photo_3 = models.ImageField(upload_to = 'photos/%Y/%m/%d/', blank=True)
+    photo_4 = models.ImageField(upload_to = 'photos/%Y/%m/%d/', blank=True)
+    photo_5 = models.ImageField(upload_to = 'photos/%Y/%m/%d/', blank=True)
+    photo_6 = models.ImageField(upload_to = 'photos/%Y/%m/%d/', blank=True)
+    is_published = models.BooleanField(default=True)
+    list_date = models.DateTimeField(default=datetime.now, blank=True)
+    
+    #this is the field that'll be displayed in the admin area
+    def __str__(self):
+        return self.title
+
+
+
+
+
+

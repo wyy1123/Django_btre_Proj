e0727993224538bd3b41e53814c1c5ec43e90fec
wyy1123 <44079135+wyy1123@users.noreply.github.com>
2023-05-08 18:47:58 -0500

    attempting to fix the statiffile dir

--- btre/settings.py
+++ btre/settings.py
@@ -14,7 +14,7 @@ from pathlib import Path
 import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent.parent
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 
 # Quick-start development settings - unsuitable for production
@@ -109,8 +109,9 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 STATIC_ROOT=os.path.join(BASE_DIR,'static')
-STATICFILES_DIRS =[
-    os.path.join(BASE_DIR,'btre/static')
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'btre/static')
 ]
 
 # Default primary key field type

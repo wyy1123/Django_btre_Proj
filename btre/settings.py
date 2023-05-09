6ab78500b191eb9869863f682ca15e7ba58dee4f
wyy1123 <44079135+wyy1123@users.noreply.github.com>
2023-05-08 19:29:47 -0500

    fixing the static not found error

--- btre/settings.py
+++ btre/settings.py
@@ -111,7 +111,7 @@ STATIC_URL = 'static/'
 STATIC_ROOT=os.path.join(BASE_DIR,'static')
 
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'btre/static')
+    os.path.join(BASE_DIR, 'btre/static'),
 ]
 
 # Default primary key field type
